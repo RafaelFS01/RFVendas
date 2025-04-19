f6e083b0c24d11e06b2188987528c2dc24cedf61
RafaelFS01 <rafafigs@outlook.com>
2025-04-18 23:33:52 -0300

    Remoção da adicionação de dependências.

--- src/main/java/BackEnd/controller/CadastrarItemController.java
+++ src/main/java/BackEnd/controller/CadastrarItemController.java
@@ -33,6 +33,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+@SuppressWarnings("ALL")
 public class CadastrarItemController implements Initializable {
 
     // --- Campos FXML Existentes ---
